15dda9435e7da9c8b275e2b7e9eb087586c90318
GoksuCeylann <72550831+GoksuCeylann@users.noreply.github.com>
2024-07-30 14:18:41 +0300

    Update CustomerSelectionScreen.js

--- CustomerSelectionScreen.js
+++ CustomerSelectionScreen.js
@@ -4,19 +4,12 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 const customers = [
+  // Add the customers in the following format
   { label: 'DİASA(MEHMET BARUN)', value: '30815', manager: 'YUSUF KOÇAK' },
   { label: 'DİLARA GIDA VE MEŞRUBAT BÜFESİ(SALİHA POLAT)', value: '30817', manager: 'YUSUF KOÇAK' },
   { label: 'TUĞBA MARKET(MUSTAFA KÜTÜK)', value: '30883', manager: 'YUSUF KOÇAK' },
   { label: 'B42 GIDA TİC.VE SAN.LTD.ŞTİ.', value: '233986', manager: 'ŞAMİL ÖZ' },
   { label: 'ÇOKDOĞRUOL GIDA - ALİ OSMAN DOĞRUOL', value: '234045', manager: 'ŞAMİL ÖZ' },
-  { label: 'GÜZEL MARKET - İLYAS KÖYLÜ', value: '231778', manager: 'ŞAMİL ÖZ' },
-  { label: 'ÇİÇEK MARKET // ERSİN ÇİÇEK', value: '25671', manager: 'MUSTAFA ASAN' },
-  { label: 'ÇİÇEK MARKET // İSMAİL ÇİÇEK', value: '25705', manager: 'MUSTAFA ASAN' },
-  { label: 'BEREKET GIDA / TURGUT KARADEMİR', value: '15533', manager: 'NURİ BOSTAN' },
-  { label: 'AKBAŞLAR MARKET / İSLAM AKBAŞ', value: '15615', manager: 'NURİ BOSTAN' },
-  { label: 'VEYSEL ÖZÇELİK', value: '195140', manager: 'ERCAN UYGUN' },
-  { label: 'TEYFİK AYÇEMAN', value: '195143', manager: 'ERCAN UYGUN' },
-  { label: 'RAMAZAN KIRKKUDU', value: '195193', manager: 'ERCAN UYGUN' },
 ];
 
 const CustomerSelectionScreen = ({ navigation, route }) => {
